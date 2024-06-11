e8eb2799e0aaff0bc1675b2f1d65a6f2425e5a40
Shanu <shanu@Shanus-MacBook-Pro.local>
2024-06-11 12:35:59 +0530

    Admin changed

--- craftdecor/webadmin/admin.py
+++ craftdecor/webadmin/admin.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 
 from .models import Categories, Products
 
-admin.site.register(Categories)
-admin.site.register(Products)
+class CategoriesAdmin(admin.ModelAdmin):
+    thumbnail = ('thumbnail')  # List of fields to make read-only
+
+class ProductsAdmin(admin.ModelAdmin):
+    thumbnail = ('thumbnail')  # List of fields to make read-only
+
+
+admin.site.register(Categories,CategoriesAdmin)
+admin.site.register(Products,ProductsAdmin)
 
